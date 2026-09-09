856edb098fe2273fd70f74fbdbb0fe6114eb1e89
Lucas Carvalho <122822747+carvolucas@users.noreply.github.com>
2026-09-09 02:41:54 -0300

    Feature: Sidebar + Header
    Sidebar e Header base finalizados

--- frontend/components/layout/Sidebar.jsx
+++ frontend/components/layout/Sidebar.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Sidebar() {
     return (
-        <nav>
-            <ul>
+        <nav className="w-64 h-screen p-4">
+            <ul className="flex flex-col gap-4 list-none ">
                 <li><Link href="/dashboard">Dashboard</Link></li>
                 <li><Link href="/projetos">Projetos</Link></li>
                 <li><Link href="/membros">Membros</Link></li>
