852c0a0819fbb6111fa1775c5f8a346c2a5d60c8
andreary1 <andreary599@gmail.com>
2026-09-10 12:03:09 -0300

    feat: models for list, task, taskAssignee, tag and taskTag

--- app/Models/models.py
+++ app/Models/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint, Uuid
+from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint, Uuid, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from uuid import UUID, uuid4
-
+from enum import Enum
 from app.database.database import Base
 
 # class Book(Base):
@@ -28,6 +28,17 @@ from app.database.database import Base
 #         self.author = author
 #         self.genre = genre
 #         self.launch_date = launch_date
+
+class TaskPriority(Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+    URGENT = "urgent"
+
+class TaskStatus(Enum):
+    PENDING = "pending"
+    DONE = "done"
+    
 class User(Base):
     __tablename__ = "users"
     id: Mapped[UUID] = mapped_column(Uuid, primary_key=True, default=uuid4)
@@ -72,6 +83,7 @@ class Project(Base):
     id: Mapped[UUID] = mapped_column(Uuid, primary_key=True, default=uuid4)
     workspace_id: Mapped[UUID] = mapped_column(Uuid, ForeignKey("workspaces.id"), nullable=False)
     workspace: Mapped["Workspace"] = relationship(back_populates="projects")
+    lists: Mapped[list["List"]] = relationship(back_populates="project")
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(String(100), nullable=True)
     created_at: Mapped[datetime] = mapped_column(
@@ -82,3 +94,47 @@ class Project(Base):
     color: Mapped[str] = mapped_column(String(100), nullable=False)
     icon_url: Mapped[str] = mapped_column(String(100), nullable=True)
 
+class List(Base):
+    __tablename__ = "lists"
+    id: Mapped[UUID] = mapped_column(Uuid,primary_key=True,default=uuid4)
+    project_id: Mapped[UUID] = mapped_column(Uuid,ForeignKey("projects.id"),nullable=False)
+    project: Mapped["Project"] = relationship(back_populates="lists")
+    tasks: Mapped[list["Task"]] = relationship(back_populates="list")
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id: Mapped[UUID] = mapped_column(Uuid,primary_key=True,default=uuid4)
+    list_id: Mapped[UUID] = mapped_column(Uuid,ForeignKey("lists.id"),nullable=False)
+    task_tags: Mapped[list["TaskTag"]] = relationship(back_populates="task")
+    list: Mapped["List"] = relationship(back_populates="tasks")
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(100), nullable=True)
+    assignees: Mapped[list["TaskAssignee"]] = relationship(back_populates="task")
+    term: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    priority: Mapped[TaskPriority] = mapped_column(Enum(TaskPriority), nullable=False, default=TaskPriority.MEDIUM)
+    status: Mapped[TaskStatus] = mapped_column(Enum(TaskStatus), nullable=False, default=TaskStatus.PENDING)
+
+class TaskAssignee(Base):
+    __tablename__ = "task_assignees"
+    id: Mapped[UUID] = mapped_column(Uuid,primary_key=True,default=uuid4)
+    task_id: Mapped[UUID] = mapped_column(Uuid,ForeignKey("tasks.id"),nullable=False)
+    user_id: Mapped[UUID] = mapped_column(Uuid, ForeignKey("users.id"), nullable=False)
+    task: Mapped["Task"] = relationship(back_populates="assignees")
+    user: Mapped["User"] = relationship(back_populates="task_assignments")
+
+class Tag(Base):
+    __tablename__ = "tags"
+    id: Mapped[UUID] = mapped_column(Uuid,primary_key=True,default=uuid4)
+    task_id: Mapped[UUID] = mapped_column(Uuid,ForeignKey("tasks.id"),nullable=False)
+    task_tags: Mapped[list["TaskTag"]] = relationship(back_populates="tag")
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    color: Mapped[str] = mapped_column(String(100), nullable=False)
+
+class TaskTag(Base):
+    __tablename__ = "task_tags"
+    id: Mapped[UUID] = mapped_column(Uuid,primary_key=True,default=uuid4)
+    task_id: Mapped[UUID] = mapped_column(Uuid,ForeignKey("tasks.id"),nullable=False)
+    tag_id: Mapped[UUID] = mapped_column(Uuid, ForeignKey("tags.id"), nullable=False)
+    task: Mapped["Task"] = relationship(back_populates="task_tags")
+    tag: Mapped["Tag"] = relationship(back_populates="task_tags")
